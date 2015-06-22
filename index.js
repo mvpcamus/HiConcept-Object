fd01275cd645c11cc5ff483dfe4e89c6e1e773e3
Jun Jo <camus@etri.re.kr>
2015-06-22 15:03:28 +0900

    packaging as a npm module - add external API functions @ index.js - usage: copy into npm modules directory & require 'Hiconcept-Object'

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+var config = require(__dirname+'/config/config.json');
+var publish = require(__dirname+'/lib/publish.js');
+var subscribe = require(__dirname+'/lib/subscribe.js');
+
+config.auth = config.name+':'+config.uuid;
+delete config.name;
+delete config.uuid;
+config.path = '/topics/'+config.topic;
+delete config.topic;
+
+exports.sub = function() {
+    subscribe(config);
+}
+
+exports.pub = function(content) {
+    publish(config, contents);
+}
